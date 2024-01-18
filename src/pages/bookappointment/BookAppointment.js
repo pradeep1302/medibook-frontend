563e2119264b40590f19ccf6e62cdca4f1a72754
Pradeep Agrawal <pkagrawal181@gmail.com>
2024-01-18 10:04:44 +0530

    rectified datepicker

--- src/pages/bookappointment/BookAppointment.js
+++ src/pages/bookappointment/BookAppointment.js
@@ -13,7 +13,10 @@ import { useNavigate, useParams } from "react-router-dom";
 import { SpinnerImg } from "../../components/loader/Loader";
 import { toast } from "react-toastify";
 import axios from "axios";
-import DatePickermui from "../../components/mui/DatePickermui";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import dayjs, { Dayjs } from "dayjs";
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
 const API_URL = `${BACKEND_URL}/api/appointments`;
@@ -111,123 +114,139 @@ export default function BookAppointment() {
     }
   };
   return (
-    <div className="col-md-7 col-lg-8" style={{ fontSize: "20px" }}>
-      <h4 className="mb-3">Book Appointment</h4>
-      <>
-        {profile === null || doctor === null ? (
-          <></>
-        ) : (
-          <form onSubmit={register}>
-            <div className="row g-3">
-              <div>
-                <label htmlFor="firstName" className="form-label">
-                  Patient Name
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="firstName"
-                  placeholder={profile.name}
-                  value=""
-                  style={{ fontSize: "20px" }}
-                  disabled
-                />
-              </div>
-              <div className="col-12">
-                <label htmlFor="dob" className="form-label">
-                  DOB
-                </label>
-                <input
-                  type="string"
-                  className="form-control"
-                  id="dob"
-                  placeholder={profile.dob}
-                  style={{ fontSize: "20px" }}
-                  disabled
-                />
-              </div>
-
-              <div className="col-12">
-                <label htmlFor="phone" className="form-label">
-                  Phone
-                </label>
-                <div className="input-group has-validation">
-                  <span
-                    className="input-group-text"
-                    style={{ fontSize: "20px" }}
-                  >
-                    +91
-                  </span>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <div className="col-md-7 col-lg-8" style={{ fontSize: "20px" }}>
+        <h4 className="mb-3">Book Appointment</h4>
+        <>
+          {profile === null || doctor === null ? (
+            <></>
+          ) : (
+            <form onSubmit={register}>
+              <div className="row g-3">
+                <div>
+                  <label htmlFor="firstName" className="form-label">
+                    Patient Name
+                  </label>
                   <input
-                    type="number"
+                    type="text"
                     className="form-control"
-                    id="phone"
-                    placeholder={profile.phone}
+                    id="firstName"
+                    placeholder={profile.name}
+                    value=""
                     style={{ fontSize: "20px" }}
                     disabled
                   />
                 </div>
+                <div className="col-12">
+                  <label htmlFor="dob" className="form-label">
+                    DOB
+                  </label>
+                  <input
+                    type="string"
+                    className="form-control"
+                    id="dob"
+                    placeholder={profile.dob}
+                    style={{ fontSize: "20px" }}
+                    disabled
+                  />
+                </div>
+
+                <div className="col-12">
+                  <label htmlFor="phone" className="form-label">
+                    Phone
+                  </label>
+                  <div className="input-group has-validation">
+                    <span
+                      className="input-group-text"
+                      style={{ fontSize: "20px" }}
+                    >
+                      +91
+                    </span>
+                    <input
+                      type="number"
+                      className="form-control"
+                      id="phone"
+                      placeholder={profile.phone}
+                      style={{ fontSize: "20px" }}
+                      disabled
+                    />
+                  </div>
+                </div>
+
+                <div className="col-12">
+                  <label htmlFor="email" className="form-label">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    placeholder={profile.email}
+                    style={{ fontSize: "20px" }}
+                    disabled
+                  />
+                </div>
+
+                <div className="col-12">
+                  <label htmlFor="address" className="form-label">
+                    Address
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="address"
+                    placeholder={profile.address}
+                    style={{ fontSize: "20px" }}
+                    disabled
+                  />
+                </div>
+                <div className="col-12">
+                  <label htmlFor="doctor" className="form-label">
+                    Doctor
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="doctor"
+                    placeholder={doctor.name}
+                    style={{ fontSize: "20px" }}
+                    disabled
+                  />
+                </div>
+                <div className="col-12" style={{ marginTop: "25px" }}>
+                  <DatePicker
+                    label="Choose a Date"
+                    minDate={dayjs(new Date())}
+                    sx={{
+                      label: {
+                        fontSize: "15px",
+                        backgroundColor: "white",
+                        padding: "0px 10px",
+                      },
+                      div: {
+                        fontSize: "20px",
+                      },
+                    }}
+                    value={dayjs(formData.date)}
+                    onChange={(newValue) =>
+                      setformData({ ...formData, date: newValue.$d })
+                    }
+                    format="DD/MM/YYYY"
+                  />
+                  {status && <p style={{ color: "green" }}>Available</p>}
+                  {!status && <p style={{ color: "red" }}>Not Available</p>}
+                </div>
               </div>
 
-              <div className="col-12">
-                <label htmlFor="email" className="form-label">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  className="form-control"
-                  id="email"
-                  placeholder={profile.email}
-                  style={{ fontSize: "20px" }}
-                  disabled
-                />
-              </div>
+              <hr className="my-4" />
 
-              <div className="col-12">
-                <label htmlFor="address" className="form-label">
-                  Address
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="address"
-                  placeholder={profile.address}
-                  style={{ fontSize: "20px" }}
-                  disabled
-                />
-              </div>
-              <div className="col-12">
-                <label htmlFor="doctor" className="form-label">
-                  Doctor
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="doctor"
-                  placeholder={doctor.name}
-                  style={{ fontSize: "20px" }}
-                  disabled
-                />
-              </div>
-              <div className="col-12" style={{ marginTop: "25px" }}>
-                <DatePickermui
-                  formData={formData}
-                  setformData={setformData}
-                  label="Choose a Date"
-                />
-                {status && <p style={{ color: "green" }}>Available</p>}
-                {!status && <p style={{ color: "red" }}>Not Available</p>}
-              </div>
-            </div>
-
-            <hr className="my-4" />
-
-            <button className="w-100 btn btn-primary btn-lg" type="submit">
-              Book Appointment
-            </button>
-          </form>
-        )}
-      </>
-    </div>
+              <button className="w-100 btn btn-primary btn-lg" type="submit">
+                Book Appointment
+              </button>
+            </form>
+          )}
+        </>
+      </div>
+    </LocalizationProvider>
   );
 }
